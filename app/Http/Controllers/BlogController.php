32f4187c5ff1f9fd8aadc435d75701642041e4fc
qxy <qiuxiaoyong@linghit.com>
2017-11-13 11:24:26 +0800

    misc

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,9 @@ use Carbon\Carbon;
 class BlogController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $posts = Post::where('published_at', '<=', Carbon::now())
